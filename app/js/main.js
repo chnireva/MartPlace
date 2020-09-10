b657f6f53919b9de34df2023c0ce4d2dc8d36441
chnireva <danilaverin97@mail.ru>
2020-09-10 17:36:56 +0500

    added section slider-weekly

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,25 @@
 $(function () {
 
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "15px",
+    readOnly: true,
+    ratedFill: "#ffc000"
+  });
 
+  $('.slider__inner-content').slick({
+    dots: false,
+    arrows: true,
+    prevArrow:
+      '<button class="slick-arrow slick-prev"><img src="images/slider-weekly/arrow-left.png" alt=""></button>',
+    nextArrow:
+      '<button class="slick-arrow slick-next"><img src="images/slider-weekly/arrow-right.png" alt=""></button>',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // autoplay: 3000
+  });
+
+  var mixer = mixitup('.products__inner-box');
 
 
 });
