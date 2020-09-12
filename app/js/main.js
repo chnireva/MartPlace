a87cf0ec5193e5059bb19df93ae515f6269aa7da
chnireva <danilaverin97@mail.ru>
2020-09-12 16:27:03 +0500

    added section slider-followers

--- app/js/main.js
+++ app/js/main.js
@@ -20,6 +20,18 @@ $(function () {
     // autoplay: 3000
   });
 
+  $('.slider__followers-inner').slick({
+    dots: false,
+    arrows: true,
+    prevArrow:
+      '<button class="slick-arrow slick-prev"><img src="images/slider-weekly/arrow-left.png" alt=""></button>',
+    nextArrow:
+      '<button class="slick-arrow slick-next"><img src="images/slider-weekly/arrow-right.png" alt=""></button>',
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    // autoplay: 3000
+  });
+
   var mixer = mixitup('.products__inner-box');
 
 
